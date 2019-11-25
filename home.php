08e881a97e5047788f867b1adcbdbe3b54e6f9b3
Mextro <mdenouden@student.scalda.nl>
2019-11-25 11:50:47 +0100

    Init

---
+++ home.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ * @package WordPress
+ */
+?>
+
+<?php get_header(); ?>
+
+    <div class="container">
+        <div class="block1">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
+                <div class="block1content">
+                    <?php the_content();?>
+                </div>
+                <a href="<?php the_permalink(); echo $about_link; ?>" class="perma-button"><?php echo $about; ?></a>
+            <?php endwhile;
+            else:?>
+                <p>Sorry, no posts matched your criteria.</p>
+            <?php endif; ?>
+        </div>
+        <div class="block2">
+            <div class="row footer-widget-wrapper">
+                <?php
+                if(is_active_sidebar('index-widget-1')){?>
+                    <div class="col-sm-4"><?php dynamic_sidebar('index-widget-1');?></div><?php
+                }
+
+                if(is_active_sidebar('index-widget-2')){?>
+                    <div class="col-sm-4"><?php dynamic_sidebar('index-widget-2');?></div><?php
+                }
+
+                if(is_active_sidebar('index-widget-3')){?>
+                    <div class="col-sm-4"><?php dynamic_sidebar('index-widget-3');?></div><?php
+                }
+
+                ?>
+            </div>
+        </div>
+        <br>
+<br><br>
+
+<!--        --><?php //echo do_shortcode('[Best_Wordpress_Gallery id="2"]'); ?>
+
+
+
+
+
+        <!--Index Post Block-->
+        <div class="block3">
+            <?php
+                $category_id = 3;
+                ?>
+            <a href="<?php the_permalink(); ?>/blog/#content"><h2 class="cat-name"><?php echo get_cat_name( $category_id );?> </h2></a>
+            <h3 class="cat-description">Recent toegevoegde Auto's</h3>
+            <?php
+
+
+            $catquery = new WP_Query( 'cat='. $category_id . '&posts_per_page=2' );
+
+            if ( $catquery->have_posts() ) :
+                while($catquery->have_posts()) : $catquery->the_post();
+                           ?>
+
+                    <?php if ($catquery->current_post % 2 == 0): ?>
+<!--                        Post Content Start-->
+                        <div class="row post-block" id="post-<?php the_ID(); ?>">
+                            <div class="col-6">
+<!--                                 Title-->
+                                <a href="<?php the_permalink(); ?>#content"> <h4 class="post-title"><?php the_title(); ?></h4></a>
+
+                                <p class="post-subtitle">
+                                    <?php echo $label_1; the_field('carrosserie'); ?><br>
+                                    <?php echo $label_2; the_field('transmissie'); ?><br>
+                                    <?php echo $label_3; the_field('kilometerstand'); ?>KM<br>
+                                    <?php echo $label_4; the_field('bouwjaar'); ?><br>
+                                    <?php echo $label_5; the_field('kenteken'); ?><br>
+                                    <?php echo $label_6; the_field('prijs'); ?>€<br>
+                                </p>
+                                <a href="<?php the_permalink(); ?>#content" class="single-button">Bekijk</a>
+                            </div>
+                            <div class="col-6">
+<!--                                 Thumbnail-->
+                                <a href="<?php the_permalink(); ?>#content"><div class="post-thumbnail"><?php the_post_thumbnail('medium_large'); ?></div></a>
+<!--                                 Thumbnail End-->
+                            </div>
+                        </div>
+<!--                        Post Content End-->
+                    <?php else: ?>
+                            <div class="row post-block" id="post-<?php the_ID(); ?>">
+                                <div class="col-6">
+<!--                                     Thumbnail-->
+                                    <a href="<?php the_permalink(); ?>#content"><div class="post-thumbnail"><?php the_post_thumbnail('medium_large'); ?></div></a>
+<!--                                     Thumbnail End-->
+                                </div>
+                                <div class="col-6">
+<!--                                     Title-->
+                                    <a href="<?php the_permalink(); ?>#content"><h4 class="post-title"><?php the_title(); ?></h4></a>
+                                    <p class="post-subtitle">
+                                        <?php echo $label_1; the_field('carrosserie'); ?><br>
+                                        <?php echo $label_2; the_field('transmissie'); ?><br>
+                                        <?php echo $label_3; the_field('kilometerstand'); ?><br>
+                                        <?php echo $label_4; the_field('bouwjaar'); ?><br>
+                                        <?php echo $label_5; the_field('kenteken'); ?><br>
+                                        <?php echo $label_6; the_field('prijs'); ?>€<br>
+                                    </p>
+                                    <a href="<?php the_permalink(); ?>#content" class="single-button">Bekijk</a>
+                                </div>
+                            </div>
+<!--                            Post Content End-->
+
+                            <?php
+
+                            endif;?>
+                <?php
+                endwhile;?>
+                <a href="<?php the_permalink(); ?>/blog/#content" class="more-posts">Meer Auto's</a>
+            <?php
+            else:
+                echo '<h4 id="results">Er zijn op dit moment geen beschikbare posts.</h4>';
+            endif; ?>
+            <?php wp_reset_query(); // reset the query ?>
+        </div>
+<!--Container End-->
+
+
+
+
+
+    </div>
+
+
+
+
+<?php get_footer(); ?>
---
+++ language-strings.php
@@ -0,0 +1,79 @@
+<?php
+
+$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+
+//Declare Variable as Global
+global $about;
+global $gallery;
+global $about_link;
+global $return_home;
+global $error_message;
+global $blog_error;
+global $searchbar;
+global $posts_per_pagelabel;
+global $filter_1;
+global $showall;
+global $view_post_list;
+global $label_1;
+global $label_2;
+global $label_3;
+global $label_4;
+global $label_5;
+global $label_6;
+global $noresults;
+global $noresultsfor;
+global $results_1;
+global $results_2;
+global $result_1;
+global $result_2;
+global $back;
+
+if (strpos($url,'/en/') !== false) {
+    //English Strings
+    $about = "Read more";
+    $gallery = "Gallery";
+    $about_link = "about";
+    $return_home = "Click here to return to the Home page.";
+    $error_message = "The requested page could not be found.";
+    $searchbar = "Search vehicles";
+    $posts_per_pagelabel = "Cars per page:";
+    $filter_1 = "Transmission:";
+    $showall = "All";
+    $view_post_list = "View Car";
+    $label_1 = "Body: ";
+    $label_2 = "Transmission: ";
+    $label_3 = "Mileage(KM): ";
+    $label_4 = "Year: ";
+    $label_5 = "License Plate: ";
+    $label_6 = "Price: ";
+    $noresults = "At this moment there aren't any cars available.";
+    $noresultsfor = "No results for:";
+    $results_1 = "There are "; $results_2 = ' results for: "';
+    $result_1 = "There is "; $result_2 = ' result for: "';
+    $back = "Go Back";
+
+} else {
+    //Dutch Strings
+    $about = "Lees meer";
+    $gallery = "Gellerij";
+    $about_link = "over-ons";
+    $return_home = "Klik hier om terug te gaan naar de Home pagina.";
+    $error_message = "De opgevraagde pagina kon niet worden gevonden!";
+    $blog_error = "Er zijn nog geen berichten.";
+    $searchbar = "Zoek voertuigen";
+    $posts_per_pagelabel = "Auto's per pagina:";
+    $filter_1 = "Transmissie:";
+    $showall = "Alle";
+    $view_post_list = "Bekijk Auto";
+    $label_1 = "Carrosserie: ";
+    $label_2 = "Transmissie: ";
+    $label_3 = "Kilometerstand: ";
+    $label_4 = "Bouwjaar: ";
+    $label_5 = "Kenteken: ";
+    $label_6 = "Prijs: ";
+    $noresults = "Op dit moment zijn er geen auto's beschikbaar.";
+    $noresultsfor = "Geen resultaten voor:";
+    $results_1 = "Er zijn "; $results_2 = ' resultaten gevonden voor: "';
+    $result_1 = "Er is "; $result_2 = ' resultaat gevonden voor: "';
+    $back = "Ga Terug";
+}
